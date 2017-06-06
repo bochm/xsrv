e03cc85c6dfc3c2dee01022ad0a18a4b86565a9b
bcmxz <bcmxz@LAPTOP-IIRN7590>
2017-06-06 23:31:52 +0800

    bcmhhhhhhhhhhhhhhhhhff

--- src/main/java/cn/bx/demo/controller/DemoTableController.java
+++ src/main/java/cn/bx/demo/controller/DemoTableController.java
@@ -9,15 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.bx.bframe.entity.DataMessage;
+import cn.bx.demo.entity.Classes;
 import cn.bx.demo.service.ClassesService;
 
 @RestController
-@RequestMapping("demo/datatable")
+@RequestMapping("demo/datatable/maindetail")
 public class DemoTableController {
 	@Resource(name="ClassesService")
 	ClassesService service;
-	@RequestMapping("maindetail")
+	@RequestMapping("classes")
 	public DataMessage listMain(@RequestBody Map<String,Object> param){
 		return DataMessage.data(service.selectList("listClasses", param));
+	} 
+	@RequestMapping("student")
+	public DataMessage listDetail(@RequestBody Map<String,Object> param){
+		return DataMessage.data(service.selectList("listStudent", param));
+	} 
+	@RequestMapping("classes/add")
+	public DataMessage add(Classes classes){
+		System.out.println(classes.getStudents().size());
+		return DataMessage.data(service.selectList("listStudent", classes));
 	} 
 }
