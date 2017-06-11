839633e8f702e6dc0ac2c6848d6add911b5b94d0
bcmxz <bcmxz@LAPTOP-IIRN7590>
2017-06-11 23:15:54 +0800

    gggghhhhhhh

--- src/main/java/cn/bx/demo/service/ClassesService.java
+++ src/main/java/cn/bx/demo/service/ClassesService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
-import cn.bx.bframe.common.util.IdGen;
 import cn.bx.bframe.entity.DataMessage;
 import cn.bx.bframe.service.SimpleService;
 import cn.bx.demo.entity.Classes;
@@ -14,20 +13,23 @@ import cn.bx.demo.entity.Student;
 public class ClassesService extends SimpleService<Classes>{
 	public DataMessage addClass(Classes classes){
 		if(this.insert("addClass", classes) > 0){
-			IdGen idGen = new IdGen(0,0);
 			for(Student student : classes.getStudents()){
-				student.setId(idGen.getId());
 				student.setClassId(classes.getId());
 			}
-			if(!CollectionUtils.isEmpty(classes.getStudents())) this.insert("addStudents", classes.getStudents());
+			if(!CollectionUtils.isEmpty(classes.getStudents())) 
+				this.insert("addStudents", classes.getStudents());
 			return DataMessage.success("新增成功", classes);
 		}
 		return DataMessage.error("新增失败", classes);
 	}
 	public DataMessage updateClass(Classes classes){
 		if(this.update("updateClass", classes) > 0){
-			if(!CollectionUtils.isEmpty(classes.getStudents()))
-				this.update("updateStudents", classes.getStudents());
+			this.delete("deleteStudents",new String[]{classes.getId()});
+			for(Student student : classes.getStudents()){
+				student.setClassId(classes.getId());
+			}
+			if(!CollectionUtils.isEmpty(classes.getStudents())) 
+				this.insert("addStudents", classes.getStudents());
 			return DataMessage.success("修改成功", classes);
 		}
 		return DataMessage.error("修改失败", classes);
